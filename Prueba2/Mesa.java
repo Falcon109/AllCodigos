73eff97e8a31cd97bff99ba72c25a0f6fb95c254
Falcon_109 <benja.fuente123@gmail.com>
2022-11-25 20:34:42 -0300

    OwO

---
+++ Prueba2/Mesa.java
@@ -0,0 +1,43 @@
+package Prueba2;
+
+public class Mesa {
+     
+    private boolean[] tenedores;
+     
+    public Mesa(int numTenedores){
+        this.tenedores = new boolean[numTenedores];
+    }
+     
+    public int tenedorIzquierda(int i){
+        return i;
+    }
+     
+    public int tenedorDerecha(int i){
+        if(i == 0){
+            return this.tenedores.length - 1;
+        }else{
+            return i - 1;
+        }
+    }
+     
+    public synchronized void cogerTenedores(int comensal, Object Logger){
+         
+        while(tenedores[tenedorIzquierda(comensal)] || tenedores[tenedorDerecha(comensal)]){
+            try {   
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Mesa.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+         
+        tenedores[tenedorIzquierda(comensal)] = true;
+        tenedores[tenedorDerecha(comensal)] = true;
+    }
+     
+    public synchronized void dejarTenedores(int comensal){
+        tenedores[tenedorIzquierda(comensal)] = false;
+        tenedores[tenedorDerecha(comensal)] = false;
+        notifyAll();
+    }
+     
+}
